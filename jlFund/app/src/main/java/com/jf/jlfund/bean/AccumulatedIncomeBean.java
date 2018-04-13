24db501c7be205dde263931c2e190b9d870e6d65
abc <abc@126.com>
2018-04-13 17:19:03 +0800

    删除代码

--- jlFund/app/src/main/java/com/jf/jlfund/bean/AccumulatedIncomeBean.java
+++
@@ -1,9 +0,0 @@
-package com.jf.jlfund.bean;
-
-/*
-* 描    述：<br>
-* 作    者：崔朋朋<br>
-* 时    间：2018/1/8<br>
-*/
-public class AccumulatedIncomeBean {
-}
--- jlFund/pickerview/src/main/java/com/airsaid/pickerviewlibrary/listener/OnItemSelectedListener.java
+++
@@ -1,6 +0,0 @@
-package com.airsaid.pickerviewlibrary.listener;
-
-
-public interface OnItemSelectedListener {
-    void onItemSelected(int index);
-}
