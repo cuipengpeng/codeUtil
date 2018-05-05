21a06091e1aa5f20dd60a7686f0d20314de8a78b
c123 <c123@163.com>
2018-05-06 01:03:07 +0800

    optimized the python multi-channel

--- pythonAndroidChannel/AndroidChannel.py
+++ pythonAndroidChannel/AndroidChannel.py
@@ -1,32 +1,44 @@
 #coding: utf-8 
-
 import shutil,os
 import zipfile
 
-apkFileName = "app-debug.apk"
-blankFile = "channelBlankFile"
+channelFileName='channel.txt'
 
-with open('channelList', 'w') as f:
+#创建一个空文件（不存在则创建）
+blankFile = "BlankFile"
+with open(blankFile, 'w') as f:
+    f.close()
+
+#clear channel content
+with open(channelFileName, 'w') as f:
     f.write('')
-with open('channelList', 'a+') as f:
-    for i in range(1,10):
+    
+with open(channelFileName, 'a+') as f:
+    for i in range(1,5):
         f.write('test'+str(i)+'\n')
 
-with open('channelList', 'r') as f:
-    line = f.readline()               
-    while line:  
-        channelName = line.strip()
-        print (channelName)
-        filename = apkFileName[0:apkFileName.rindex('.')]
-        newFileName = filename+"-"+channelName+".apk"
-        print (newFileName)
-        shutil.copy(apkFileName,newFileName)
-        
-        zipped = zipfile.ZipFile(newFileName, 'a', zipfile.ZIP_DEFLATED) 
-        empty_channel_file = "META-INF/channel_{channel}".format(channel=channelName)
-        zipped.write(blankFile, empty_channel_file)
-        zipped.close()
-        line = f.readline()  
+# 获取当前目录中所有的apk源包
+apkList = []
+# python3 : os.listdir()即可，这里使用兼容Python2的os.listdir('.')
+for file in os.listdir('.'):
+    if os.path.isfile(file):
+        extension = os.path.splitext(file)[1][1:]
+        if extension == 'apk':
+            apkList.append(file)
 
-
-
+for apkFileName in apkList:
+    print (apkFileName+"====")
+    with open(channelFileName, 'r') as f:
+        line = f.readline()               
+        while line:  
+            channelName = line.strip()
+            filename = apkFileName[0:apkFileName.rindex('.')]
+            newFileName = filename+"-"+channelName+".apk"
+            print (newFileName)
+            shutil.copy(apkFileName,newFileName)
+            
+            zipped = zipfile.ZipFile(newFileName, 'a', zipfile.ZIP_DEFLATED) 
+            empty_channel_file = "META-INF/channel_{channel}".format(channel=channelName)
+            zipped.write(blankFile, empty_channel_file)
+            zipped.close()
+            line = f.readline()  
