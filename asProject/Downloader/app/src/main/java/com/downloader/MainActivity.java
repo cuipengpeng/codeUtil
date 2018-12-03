14a878f0c88d17df915421c5f83b26d2cb862769
pengpeng.cui <pengpeng.cui@9icaishi.net>
2018-12-03 21:56:19 +0800

    add  multi download url

--- asProject/Downloader/app/src/main/java/com/downloader/MainActivity.java
+++ asProject/Downloader/app/src/main/java/com/downloader/MainActivity.java
@@ -71,10 +71,17 @@ public class MainActivity extends Activity {
 
 //    	String address = addressET.getText().toString().trim();
     	String address = "http://down.360safe.com/se/360se9.1.0.426.exe";
-//    	String address = "http://ftp.yz.yamagata-u.ac.jp/pub/eclipse/oomph/epp/2018-09/Ra/eclipse-inst-win64.exe";
+    	String address01 = "http://ftp.yz.yamagata-u.ac.jp/pub/eclipse/oomph/epp/2018-09/Ra/eclipse-inst-win64.exe";
+    	String address02 = "http://ftp.yz.yamagata-u.ac.jp/pub/eclipse/oomph/epp/2018-09/Ra/eclipse-inst-linux64.tar.gz";
+    	String address03 = "http://ftp.yz.yamagata-u.ac.jp/pub/eclipse/oomph/epp/2018-09/Ra/eclipse-inst-mac64.tar.gz";
+		String address04 = "http://dldir1.qq.com/weixin/android/weixin657android1040.apk";
+		String address05 = "https://qd.myapp.com/myapp/qqteam/AndroidQQ/mobileqq_android.apk";
 //    	String address = "http://video.chaogevideo.com/download/chaoge/01/chaoge/v1.0/chaoge_v1.0_yueming.apk";
 
 		BreakpointDownloader.getInstance().downloadBigFile(address, handler);
+//		BreakpointDownloader.getInstance().downloadBigFile(address01, handler);
+//		BreakpointDownloader.getInstance().downloadBigFile(address02, handler);
+//		BreakpointDownloader.getInstance().downloadBigFile(address03, handler);
 //		BreakpointDownloader.getInstance().downloadSmallFile(address, null, 1, new Handler());
     }
 }
