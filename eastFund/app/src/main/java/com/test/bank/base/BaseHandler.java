706cd3b94e3fbb0e9e3479cd45609c7e5f8e830f
pengpeng.cui <pengpeng.cui@9icaishi.net>
2019-03-27 14:17:03 +0800

    add BaseHandler

---
+++ eastFund/app/src/main/java/com/test/bank/base/BaseHandler.java
@@ -0,0 +1,34 @@
+package com.test.bank.base;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * Handler的封装
+ * 子类应为静态内部类
+ *
+ * @param <T> 外部类
+ */
+public abstract class BaseHandler<T> extends Handler {
+    private final WeakReference<T> mReference;
+
+    //handler.removeCallbacksAndMessages(this)
+    public BaseHandler(T t) {
+        super(Looper.getMainLooper());
+        mReference = new WeakReference<T>(t);
+    }
+
+    @Override
+    public void handleMessage(Message msg) {
+        T t = mReference.get();
+        if (t != null) {
+            handleMessage(t, msg);
+        }
+    }
+
+    protected abstract void handleMessage(T t, Message msg);
+}
+
