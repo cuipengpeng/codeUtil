d567f13563aa1d9c7cb03789b918888997ea7b4b
abcd <abcd123@123.com>
2019-08-23 00:08:37 +0800

    optimized  MultiThreadShareData.java

--- JavaTest01/src/util/MultiThreadShareData.java
+++ JavaTest01/src/util/MultiThreadShareData.java
@@ -3,14 +3,20 @@ package util;
 public class MultiThreadShareData {
 	
 	public static void main(String[] args) {
-	        final ShareData1 shareData1 = new ShareData1();
+	        final ShareData1 shareData1 = new ShareData1(0, "qqqqqq");
 	        new Thread(new MyRunnable1(shareData1)).start();
-	        new Thread(new MyRunnable1(shareData1)).start();
+	        new Thread(new MyRunnable2(shareData1)).start();
 	}
 	
 	static class MyRunnable1 implements Runnable{
+		public static final String TAG="MyRunnable-1";
 	        private ShareData1 shareData1;
 	        public void run() {
+	        	System.out.println(TAG+"--mCount="+shareData1.mCount+"--"+shareData1.mName);
+	        	shareData1.mCount = shareData1.mCount+1;
+	        	shareData1.mName = "name-1";
+	        	System.out.println(TAG+"--mCount="+shareData1.mCount+"--"+shareData1.mName);
+
 	        }
 	        public MyRunnable1(ShareData1 shareData1){
 	            this.shareData1 = shareData1;
@@ -18,8 +24,14 @@ public class MultiThreadShareData {
 	    }
 	
 	static class MyRunnable2 implements Runnable{
+		public static final String TAG="MyRunnable-2";
 	        private ShareData1 shareData1;
 	        public void run() {
+	        	System.out.println(TAG+"--mCount="+shareData1.mCount+"--"+shareData1.mName);
+	        	shareData1.mCount = shareData1.mCount+1;
+	        	shareData1.mName = "name-2";
+	        	System.out.println(TAG+"--mCount="+shareData1.mCount+"--"+shareData1.mName);
+
 	        }
 	        public MyRunnable2(ShareData1 shareData1){
 	            this.shareData1 = shareData1;
@@ -27,5 +39,23 @@ public class MultiThreadShareData {
 	    }
 	
 	static class ShareData1 {
+		int mCount=0;
+		String mName="abc";
+
+		public ShareData1(int count, String name) {
+			this.mCount = count;
+			this.mName = name;
+		}
+		
+		public void get() {
+			synchronized (this) {
+				
+			}
+		}
+		public void set() {
+			synchronized (this) {
+				
+			}
+		}
 	}
 }
